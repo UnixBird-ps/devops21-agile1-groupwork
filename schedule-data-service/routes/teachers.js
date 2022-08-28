ad04b2da90c69334c85585de7a1f37da77eb04ca
nodeFadel <fadel.a.bourji@gmail.com>
2022-08-28 17:55:41 +0200

    edit route teacher file

--- schedule-data-service/routes/teachers.js
+++ schedule-data-service/routes/teachers.js
@@ -1,10 +1,93 @@
+// const encrypt = require('../modules/encrypt.js')
+
+// module.exports = function(server, db){
+
+//   server.get('/data/teachers', (req, res)=>{
+//     let query = "SELECT id, firstname, lastname, initials, phone, email, color, hide  FROM teachers ORDER BY initials"
+//     let result = db.prepare(query).all()
+//     res.json(result)
+//   })
+
+//   // registrera en ny lärare
+//   server.post('/data/teachers', (request, response) => {
+//     let user = request.body
+//     let encryptedPassword = encrypt(user.password)
+//     let result
+//     try{
+//       result = db.prepare('INSERT INTO teachers (email, password) VALUES(?,?)').run([user.email, encryptedPassword])
+//     }catch(e){
+//       console.error(e)
+//     }
+//     response.json(result)
+//   })
+
+
+//   // komplettera profil för användare
+//   server.put('/data/teachers', (request, response) => {
+//     let user = request.body
+//     let result
+//     try{
+//       result = db.prepare('UPDATE teachers SET firstname = ?, lastname = ?, initials = ?, phone = ?, color = ?, hide = ? WHERE email = ?').run([user.firstname, user.lastname, user.initials, user.phone, user.color, user.hide, user.email])
+//     }catch(e){
+//       console.error(e)
+//     }
+//     response.json(result)
+//   })
+
+
+//   // begär ändring av lösenord för användare
+//   server.delete('/data/teachers/password', (request, response) => {
+//     let user = request.body
+//     let result
+//     try{
+//       result = db.prepare('UPDATE teachers SET password = NULL WHERE email = ? AND hide = 0').run([user.email])
+//     }catch(e){
+//       console.error(e)
+//     }
+//     response.json(result)
+//   })
+
+
+//   // ändra lösenord för användare
+//   server.patch('/data/teachers/password', (request, response) => {
+//     let user = request.body
+//     let encryptedPassword = encrypt(user.password)
+//     let result
+//     try{
+//       result = db.prepare('UPDATE teachers SET password = ? WHERE password IS NULL AND email = ?').run([encryptedPassword, user.email])
+//     }catch(e){
+//       console.error(e)
+//     }
+//     response.json(result)
+//   })
+
+
+//}
+
+
+
+
+
 const encrypt = require('../modules/encrypt.js')
 
-module.exports = function(server, db){
+module.exports = function (server, db) {
 
-  server.get('/data/teachers', (req, res)=>{
-    let query = "SELECT id, firstname, lastname, initials, phone, email, color, hide  FROM teachers ORDER BY initials"
+  server.get('/data/teachers', (req, res) => {
+    // let query = "SELECT id, firstname, lastname, initials, phone, email, color, hide FROM teachers ORDER BY initials"
+    let query = "SELECT * FROM teachers ORDER BY initials"
     let result = db.prepare(query).all()
+    res.setHeader('Content-Range', result.length);
+    res.setHeader('X-Total-Count', result.length);
+    res.json(result)
+  }
+  )
+
+  server.get('/data/teachers/:id', (req, res) => {
+    // let query = "SELECT id, firstname, lastname, initials, phone, email, color, hide FROM teachers WHERE id = @id"
+    let query = "SELECT * FROM teachers WHERE id = @id"
+    let result = db.prepare(query).get({ id: req.params.id })
+    // res.setHeader( 'Content-Range', result.length);
+    // res.setHeader( 'X-Total-Count', result.length);
     res.json(result)
   })
 
@@ -13,35 +96,41 @@ module.exports = function(server, db){
     let user = request.body
     let encryptedPassword = encrypt(user.password)
     let result
-    try{
+    try {
       result = db.prepare('INSERT INTO teachers (email, password) VALUES(?,?)').run([user.email, encryptedPassword])
-    }catch(e){
+    }
+    catch (e) {
       console.error(e)
     }
     response.json(result)
-  })
+  }
+  )
 
 
   // komplettera profil för användare
   server.put('/data/teachers', (request, response) => {
     let user = request.body
     let result
-    try{
-      result = db.prepare('UPDATE teachers SET firstname = ?, lastname = ?, initials = ?, phone = ?, color = ?, hide = ? WHERE email = ?').run([user.firstname, user.lastname, user.initials, user.phone, user.color, user.hide, user.email])
-    }catch(e){
+    try {
+      result = db.prepare(
+        'UPDATE teachers SET firstname = ?, lastname = ?, initials = ?, phone = ?, color = ?, hide = ? WHERE email = ?'
+      ).run([user.firstname, user.lastname, user.initials, user.phone, user.color, user.hide, user.email])
+    }
+    catch (e) {
       console.error(e)
     }
     response.json(result)
-  })
+  }
+  )
 
 
   // begär ändring av lösenord för användare
   server.delete('/data/teachers/password', (request, response) => {
     let user = request.body
     let result
-    try{
+    try {
       result = db.prepare('UPDATE teachers SET password = NULL WHERE email = ? AND hide = 0').run([user.email])
-    }catch(e){
+    } catch (e) {
       console.error(e)
     }
     response.json(result)
@@ -53,9 +142,9 @@ module.exports = function(server, db){
     let user = request.body
     let encryptedPassword = encrypt(user.password)
     let result
-    try{
+    try {
       result = db.prepare('UPDATE teachers SET password = ? WHERE password IS NULL AND email = ?').run([encryptedPassword, user.email])
-    }catch(e){
+    } catch (e) {
       console.error(e)
     }
     response.json(result)
