f3115e6a591644587cc1bac6443fed056f27bb27
nodeFadel <fadel.a.bourji@gmail.com>
2022-08-28 18:08:32 +0200

    update api-description file

--- schedule-data-service/api-description.js
+++ schedule-data-service/api-description.js
@@ -1,67 +1,67 @@
-module.exports = function(host){
+module.exports = function (host) {
     return [
         {
-            route:"/data",
+            route: "/data",
             methods: ["GET"],
-            description:"This route: The API documentation"
+            description: "This route: The API documentation"
         },
         {
-            route:"/data/calendar/:from/:to",
+            route: "/data/calendar/:from/:to",
             methods: ["GET"],
-            description:"List calendar between :from and :to (including holidays)",
-            link: host  + "/data/calendar/2022-01-01/2022-03-30"
+            description: "List calendar between :from and :to (including holidays)",
+            link: host + "/data/calendar/2022-01-01/2022-03-30"
         },
         {
-            route:"/data/populate-swedish-holidays/:from/:to'",
+            route: "/data/populate-swedish-holidays/:from/:to'",
             methods: ["GET"],
-            description:"Populate database with holidays between :from and :to. (Warning, destructive, clears all previously loaded holidays from database)",
-            link: host  + "/data/populate-swedish-holidays/2020-01-01/2028–12-31"
+            description: "Populate database with holidays between :from and :to. (Warning, destructive, clears all previously loaded holidays from database)",
+            link: host + "/data/populate-swedish-holidays/2020-01-01/2028–12-31"
         },
         {
-            route:"/data/login",
-            methods: ["POST","GET","DELETE"],
-            description:"Login user, get current logged in user, logout"
+            route: "/data/login",
+            methods: ["POST", "GET", "DELETE"],
+            description: "Login user, get current logged in user, logout"
         },
         {
-            route:"/data/teachers",
-            methods: ["GET","POST","PUT"],
-            description:"Get list of teachers, create teacher, add/change teacher details",
-            link: host  + "/data/teachers"
+            route: "/data/teachers",
+            methods: ["GET", "POST", "PUT"],
+            description: "Get list of teachers, create teacher, add/change teacher details",
+            link: host + "/data/teachers"
         },
         {
-            route:"/data/teachers/password",
-            methods: ["DELETE","PATCH"],
-            description:"Clear old password, add new password",
-            link: host  + "/data/teachers/password"
+            route: "/data/teachers/password",
+            methods: ["DELETE", "PATCH"],
+            description: "Clear old password, add new password",
+            link: host + "/data/teachers/password"
         },
         {
-            route:"/data/classes",
+            route: "/data/classes",
             methods: ["GET"],
-            description:"Get list of classes",
-            link: host  + "/data/classes"
+            description: "Get list of classes",
+            link: host + "/data/classes"
         },
         {
-            route:"/data/classes_view",
+            route: "/data/classes_view",
             methods: ["GET"],
-            description:"Get enriched list of classes",
-            link: host  + "/data/classes_view"
+            description: "Get enriched list of classes",
+            link: host + "/data/classes_view"
         },
         {
-            route:"/data/schools",
+            route: "/data/schools",
             methods: ["GET"],
-            description:"Get list of schools",
-            link: host  + "/data/schools"
+            description: "Get list of schools",
+            link: host + "/data/schools"
         },
         {
-            route:"/data/courses",
+            route: "/data/courses",
             methods: ["GET"],
-            description:"Get list of courses",
-            link: host  + "/data/courses"
+            description: "Get list of courses",
+            link: host + "/data/courses"
         },
         {
-            route:"/data/invoices",
-            methods: ["POST","GET"],
-            body: {                
+            route: "/data/invoices",
+            methods: ["POST", "GET"],
+            body: {
                 startDate: "Date (required)",
                 endDate: "Date (required)",
                 school: "INT id (required)",
@@ -72,8 +72,8 @@ module.exports = function(host){
                 yourRef: "String (optional)",
                 ourRef: "String (optional)"
             },
-            description:"Generate and get invoices",
-            link: host  + "/data/invoices"
+            description: "Generate and get invoices",
+            link: host + "/data/invoices"
         }
     ]
 }
