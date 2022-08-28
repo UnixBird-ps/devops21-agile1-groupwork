46dd9f5f0005b5ed21e6f86abf0e9340d3be57ab
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-08-28 15:01:43 +0200

    created Form for edit courses

--- admin/src/course-edit.jsx
+++ admin/src/course-edit.jsx
@@ -1,23 +1,21 @@
-import { Edit, ReferenceInput, AutocompleteInput, SelectInput, SimpleForm, TextInput, NumberInput, DateInput, BooleanInput } from 'react-admin';
+import { Edit, ReferenceInput, AutocompleteInput, SimpleForm, TextInput, NumberInput, DateInput, BooleanInput } from 'react-admin';
 
 export const CourseEdit = () => (
-  <Edit title="Edit kurs">
+  <Edit title="Edit Course">
     <SimpleForm>
       <NumberInput disabled source="id" />
       <TextInput source="name" />
       <TextInput source="shortName" />
       <ReferenceInput source="class" label="shortName" reference="classes">
-        <AutocompleteInput label="Class" />{/* shortName */}
+        <AutocompleteInput label="Class" />
       </ReferenceInput>
-      {/* <TextInput source="class" /> */}
       <NumberInput source="points" />
       <DateInput source="startDate" />
       <DateInput source="endDate" />
       <TextInput source="plan" />
       <ReferenceInput source="id" reference="invoices">
-        <AutocompleteInput label="Invoice" />{/* shortName */}
+        <AutocompleteInput label="Invoice" />
       </ReferenceInput>
-      {/* <TextInput disabled source="invoiceItem" /> */}
       <NumberInput source="hoursPerDay" />
       <BooleanInput source="hide" parse={v => v ? 1 : 0} format={ v => v == 1} />
     </SimpleForm>
