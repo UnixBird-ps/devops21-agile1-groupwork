c9062761608f6d7cc05446720fb2542d8adc6ba7
nodeFadel <fadel.a.bourji@gmail.com>
2022-08-28 17:47:48 +0200

    edit App- react

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,23 +1,30 @@
+
 // in src/App.js
 import * as React from "react";
-import { Admin, Resource, EditGuesser } from 'react-admin';
-import { PostList } from './posts';
-import { PostEdit } from "./post-edit";
-import { PostCreate} from "./post-create";
-import { UserList } from './users';
+import { Admin, Resource, ListGuesser, fetchUtils, EditGuesser, List } from 'react-admin';
+import { TeacherList } from './teachers';
+import { ClassList } from './classes';
+import { CourseList } from "./courses";
+import { CourseEdit } from "./course-edit";
+import { SchoolList } from "./schools";
 import jsonServerProvider from 'ra-data-json-server';
-import PostIcon from '@mui/icons-material/Book';
+import simpleRestProvider from 'ra-data-simple-rest';
+// import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
 import Dashboard from "./Dashboard";
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+// const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+// const dataProvider = jsonServerProvider('/data');
+const dataProvider = simpleRestProvider('/data');
 
-// <Resource name="users" list={ListGuesser} />
 
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider} disableTelemetry>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
-    <Resource name="users" list={UserList} icon={UserIcon} />
-  </Admin>
+   <Admin dataProvider={dataProvider} dashboard={Dashboard} disableTelemetry>
+      <Resource name="teachers" list={TeacherList} edit={EditGuesser} icon={UserIcon} />
+      <Resource name="classes" list={ClassList} edit={EditGuesser} />
+      <Resource name="courses" list={CourseList} edit={CourseEdit} />
+      <Resource name="schools" list={SchoolList} edit={EditGuesser} />
+      {/* <Resource name="users" list={UserList} create={PostCreate} icon={UserIcon} /> */}
+   </Admin>
 );
 
 export default App;
