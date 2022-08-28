e14c76d2a583eb72156ee3e28ed686667e9a68d1
nodeFadel <fadel.a.bourji@gmail.com>
2022-08-28 18:07:15 +0200

    update index.js file

--- schedule-data-service/index.js
+++ schedule-data-service/index.js
@@ -2,14 +2,14 @@ const db = require("./modules/db.js")()
 const express = require("express")
 const server = express()
 server.use(express.json())
-const port = 7666
+const port = process.env.PORT ? process.env.PORT : 30080;
 const host = `http://localhost:${port}`
 
 // sessions
 let cookieParser = require('cookie-parser')
 server.use(cookieParser())
 let session = require('express-session')
-server.use( session( {
+server.use(session({
   secret: 'keyboard cat jksfj<khsdka',
   resave: false,
   saveUninitialized: true,
@@ -20,14 +20,14 @@ server.use( session( {
 }))
 
 // bypass 2FA verification (dev only)
-server.use(function(req,res,next){req.bypassVerification = true; next()})
+server.use(function (req, res, next) { req.bypassVerification = true; next() })
 
 // ACL
 const acl = require('./services/acl.js')
 server.use(acl)
 
 // start
-server.listen(port,() => {
+server.listen(port, () => {
   console.log(host)
   console.log('server running on port ' + port)
 })
@@ -41,8 +41,9 @@ server.use('/assets', express.static('../admin/dist/assets'))
 
 // REST API routes
 
-require('./routes/teachers.js')(server, db)
 require('./routes/login.js')(server, db)
+require('./routes/teachers.js')(server, db)
+require('./routes/courses.js')(server, db)
 
 const apiDescription = require('./api-description.js')(host)
 
@@ -50,37 +51,24 @@ server.get("/data", async (req, res) => {
   res.json(apiDescription)
 })
 
-server.get('/data/calendar/:from/:to', (req, res)=>{
-    const cal = calendar.makeCalendar(req.params.from, req.params.to, req.params.locale)
-    const populated = calendar.populateCalendar(cal)
-    res.json(populated)
+server.get('/data/calendar/:from/:to', (req, res) => {
+  const cal = calendar.makeCalendar(req.params.from, req.params.to, req.params.locale)
+  const populated = calendar.populateCalendar(cal)
+  res.json(populated)
 })
 
-server.get('/data/courses/:from/:to', (req, res)=>{
-  let query = "SELECT * FROM courses WHERE startDate >= @startDate AND endDate <= @endDate"
-  let result = db.prepare(query).all({startDate: req.params.from, endDate: req.params.to})
-  res.json(result)
-})
-
-server.post('/data/courses', (req, res)=>{
-  let query = "INSERT INTO courses VALUES(@id, @name, @shortName, @class, @points, @startDate, @endDate, @plan, @invoiceItem, @hoursPerDay)"
-  let statement = db.prepare(query)
-  let result = statement.run(req.body)
-  res.json(result)
-})
-
-server.get('/data/classes_view/:all?', (req, res)=>{
+server.get('/data/classes_view/:all?', (req, res) => {
   let query
-  if(req.params.all){
+  if (req.params.all) {
     query = "SELECT * FROM classes_view WHERE ORDER BY schoolShortName, shortName"
-  }else{
+  } else {
     query = "SELECT * FROM classes_view WHERE hide = 0 ORDER BY schoolShortName, shortName"
-  }  
+  }
   let result = db.prepare(query).all()
 
-  for(let i=result.length-1;i>=0;i--){
-    if(result[i].schoolShortName==='Nodehill'){ // move Nodehill to the very end of the ordered set
-      let row = result.splice(i,1)
+  for (let i = result.length - 1; i >= 0; i--) {
+    if (result[i].schoolShortName === 'Nodehill') { // move Nodehill to the very end of the ordered set
+      let row = result.splice(i, 1)
       result.push(row[0])
       break;
     }
@@ -90,8 +78,8 @@ server.get('/data/classes_view/:all?', (req, res)=>{
 
 const createInvoice = require('./services/create-invoice.js')
 
-server.post('/data/invoices/', (req, res)=>{
-  if(!req.body.startDate || !req.body.endDate || !req.body.school){
+server.post('/data/invoices/', (req, res) => {
+  if (!req.body.startDate || !req.body.endDate || !req.body.school) {
     res.json({
       error: "Insufficient request data"
     })
@@ -104,8 +92,32 @@ server.post('/data/generate-schedule', generateSchedule)
 
 // generic one-to-one table API
 
-server.get('/data/:table', (req, res)=>{ // but limit which tables to query with ACL
+server.get('/data/:table', (req, res) => { // but limit which tables to query with ACL
   let query = "SELECT * FROM " + req.params.table
   let result = db.prepare(query).all()
+  res.setHeader('Content-Range', result.length);
+  res.setHeader('X-Total-Count', result.length);
   res.json(result)
-})
+}
+)
+
+server.get('/data/:table/:id', (req, res) => { // but limit which tables to query with ACL
+  let query = "SELECT * FROM " + req.params.table + " WHERE id = @id"
+  let result = db.prepare(query).get({ id: req.params.id })
+  // res.setHeader( 'Content-Range', result.length);
+  // res.setHeader( 'X-Total-Count', result.length);
+  res.json(result)
+}
+)
+
+
+// server.put('/data/classes', (request, response) => {
+//   let c = request.body
+//   let result
+//   try{
+//     result = db.prepare('UPDATE classes SET name = ?, shortName = ?, school = ?, bloghide = ?, defaultStartTime = ?, defaultEndTime = ?, defaultInvoiceItem = ?, defaultHoursPerDay = ? WHERE email = ?').run([ c.id, c.name, c.shortName, c.school, c.bloghide, c.defaultStartTime, c.defaultEndTime, c.defaultInvoiceItem, c.defaultHoursPerDay])
+//   }catch(e){
+//     console.error(e)
+//   }
+//   response.json(result)
+// })
