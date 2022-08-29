0ae20b4d6016fc610c9d20cb83409db586ef569f
nodeFadel <fadel.a.bourji@gmail.com>
2022-08-29 17:55:44 +0200

    edit user list

--- admin/src/users.jsx
+++ admin/src/users.jsx
@@ -8,7 +8,7 @@ export const UserList = () => (
       <TextField source="username" />
       <EmailField source="email" />
       <TextField source="address.street" />
-      {/* <TextField source="phone" /> */}
+      <TextField source="phone" />
       <UrlField source="website" />
       <TextField source="company.name" />
     </Datagrid>
