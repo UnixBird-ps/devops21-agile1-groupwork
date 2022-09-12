ef2c192b9dae329e4bd2697862d59f1698daa556
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-09-12 14:39:04 +0200

    Completed Gherkin for login feature

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,62 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require( "cypress" );
+const webpack = require( "@cypress/webpack-preprocessor" );
+const { addCucumberPreprocessorPlugin } = require( "@badeball/cypress-cucumber-preprocessor" );
 
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+module.exports = defineConfig(
+  {
+    e2e:
+    {
+      specPattern: "**/*.feature",
+      setupNodeEvents(on, config)
+      {
+        // implement node event listeners here
+
+        // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+        addCucumberPreprocessorPlugin(on, config);
+
+        on(
+          "file:preprocessor",
+          webpack(
+            {
+              webpackOptions:
+              {
+                resolve: {
+                  extensions: [".js"],
+                },
+                module: {
+                  rules:
+                  [
+                    {
+                      test: /\.feature$/,
+                      use:
+                      [
+                        {
+                          loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                          options: config,
+                        },
+                      ],
+                    },
+                  ],
+                },
+              },
+            }
+          )
+        );
+
+        on(
+          'task',
+          {
+            log(message)
+            {
+              console.log(message)
+              return null
+            },
+          }
+        );
+
+        // Make sure to return the config object as it might have been modified by the plugin.
+        return config;
+      }
+    }
+  }
+);
