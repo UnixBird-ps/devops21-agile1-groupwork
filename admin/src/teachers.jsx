941931a5a9c66fe6c3edb920ddd9c9646feb604c
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-09-23 23:26:24 +0200

    Fixed black color for color picker in Chrome when color was specified with 3 hex digits

--- admin/src/teachers.jsx
+++ admin/src/teachers.jsx
@@ -8,8 +8,15 @@ import { Box } from '@mui/material';
 const Separator = () => <Box pt="1em" />;
 
 
+// Used for role field and input
+const choices = [
+   { id: 'admin', role: 'admin' },
+   { id: 'user', role:  'user' }
+];
+
+
 // Returns a combined div containing the record's color value and a colored bar under the text
-const ColorField = ( pSource ) =>
+const ColorField = pSource =>
 {
   // Get the prop's parent record
   const record = useRecordContext( pSource );
@@ -23,20 +30,54 @@ const ColorField = ( pSource ) =>
 };
 
 
-// Used for role field and input
-const choices = [
-   { id: 'admin', role: 'admin' },
-   { id: 'user', role:  'user' }
-];
-
-
 // Return a <span> containing text converted from an array
-const RolesField = ( source ) =>
+const RolesField = source =>
 {
   // Get the prop's parent record
   const record = useRecordContext( source );
   return record ? <span>{ Object.values( record.roles ).join(',') }</span> : null;
 };
+
+
+const validateEditForm = values =>
+{
+  const errors = {};
+  if (!values.email) {
+      // errors.email = 'Email is required';
+      errors.email = 'ra.validation.required';
+  }
+  if (!values.color) {
+      // You can return translation keys
+      errors.color = 'ra.validation.required';
+  }
+  return errors;
+}
+
+
+const validateCreateForm = values =>
+{
+  const errors = {};
+  if (!values.email) {
+      errors.email = 'ra.validation.required';
+  }
+  if (!values.password) {
+      errors.password = 'ra.validation.required';
+  }
+  if (!values.color) {
+      errors.color = 'ra.validation.required';
+  }
+  return errors;
+}
+
+
+const colorFormatter = pValue =>
+{
+  if ( typeof pValue === 'string' && pValue.startsWith( '#' ) && pValue.length == 4 )
+  {
+    pValue = '#' + pValue.slice( 1 ).split( '' ).map( val => `${val}${val}` ).join( '' );
+  }
+  return pValue;
+}
 
 
 //  rowClick="edit" bulkActionButtons={false} hasCreate={true}
@@ -58,21 +99,6 @@ export const TeacherList = () => (
 );
 
 
-const validateEditForm = (values) =>
-{
-  const errors = {};
-  if (!values.email) {
-      // errors.email = 'Email is required';
-      errors.email = 'ra.validation.required';
-  }
-  if (!values.color) {
-      // You can return translation keys
-      errors.color = 'ra.validation.required';
-  }
-  return errors;
-}
-
-
 export const TeacherEdit = () => (
   <Edit title="Edit Teacher">
     <SimpleForm warnWhenUnsavedChanges validate={validateEditForm} sx={{ maxWidth: 500 }}>
@@ -91,28 +117,12 @@ export const TeacherEdit = () => (
       </Box>
       <TextInput source="phone" />
       <Separator />
-      <TextInput source="color" type="color" sx={{minWidth:75}} defaultValue="#888888"/>
+      <TextInput source="color" type="color" sx={{minWidth:75}} defaultValue="#888888" format={colorFormatter} />
       <SelectArrayInput source="roles" choices={choices} optionValue="role" optionText="role" />
       <BooleanInput source="hide" defaultChecked={false} />
     </SimpleForm>
   </Edit>
 );
-
-
-const validateCreateForm = (values) =>
-{
-  const errors = {};
-  if (!values.email) {
-      errors.email = 'ra.validation.required';
-  }
-  if (!values.password) {
-      errors.password = 'ra.validation.required';
-  }
-  if (!values.color) {
-      errors.color = 'ra.validation.required';
-  }
-  return errors;
-}
 
 
 export const TeacherCreate = () => (
